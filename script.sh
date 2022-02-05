5b54ef4cd8ab4f8b207c6f458037ae6645415ec9
Lapsus Ordonné <22498591+mateo-fernandez@users.noreply.github.com>
2022-02-06 00:51:16 +0100

    Delete script.sh

--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/GoogleCloudPlatform/nodejs-docs-samples.git && cd nodejs-docs-samples/appengine/hello-world/flexible
-
-sed -i 's/12.20.0/12.14.1/g' package.json
-
-npm install
-
-gcloud app deploy
