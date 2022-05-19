44f9bb29891265efb77ac9ef6208bab0335e23c9
Lapsus Ordonné <22498591+mateo-fernandez@users.noreply.github.com>
2022-05-20 00:03:19 +0200

    Update k8s script

--- scripts/install_k8s.sh
+++ scripts/install_k8s.sh
@@ -9,3 +9,8 @@ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pack
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
+
+# Minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
+sudo mkdir -p /usr/local/bin/
+sudo install minikube /usr/local/bin/
